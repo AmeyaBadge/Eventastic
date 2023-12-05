d5003cb0eb5fb3f2bff6ff433d3bf33ca4c581b4
Ameya Badge <ameyabadge@gmail.com>
2023-12-05 16:54:25 +0530

    Copied SignUp Design

---
+++ app/src/main/java/com/amykoder/eventastic/SignUpActivity.java
@@ -0,0 +1,14 @@
+package com.amykoder.eventastic;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}

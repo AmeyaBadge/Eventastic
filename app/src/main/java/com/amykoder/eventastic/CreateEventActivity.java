95b1086db977b92c26cc69e1ac8cab63ae22a083
Ameya Badge <ameyabadge@gmail.com>
2023-12-18 14:05:35 +0530

    addedLogoutFeatureAndCreateEventUnderDev

---
+++ app/src/main/java/com/amykoder/eventastic/CreateEventActivity.java
@@ -0,0 +1,15 @@
+package com.amykoder.eventastic;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CreateEventActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_event);
+
+    }
+}
--- app/src/main/java/com/amykoder/eventastic/ui/gallery/GalleryFragment.java
+++ app/src/main/java/com/amykoder/eventastic/ui/gallery/GalleryFragment.java
@@ -1,5 +1,6 @@
 package com.amykoder.eventastic.ui.gallery;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,12 +11,15 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.amykoder.eventastic.LoginActivity;
 import com.amykoder.eventastic.databinding.FragmentGalleryBinding;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class GalleryFragment extends Fragment {
 
     private FragmentGalleryBinding binding;
 
+    private FirebaseAuth mAuth;
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         GalleryViewModel galleryViewModel =
@@ -23,9 +27,15 @@ public class GalleryFragment extends Fragment {
 
         binding = FragmentGalleryBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+        mAuth = FirebaseAuth.getInstance();
 
-        final TextView textView = binding.textGallery;
-        galleryViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        binding.logoutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mAuth.signOut();
+                startActivity(new Intent(getContext(), LoginActivity.class));
+            }
+        });
         return root;
     }
 
